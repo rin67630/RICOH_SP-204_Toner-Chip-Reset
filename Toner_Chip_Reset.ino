18e65d230ff73c972c79b3bddd4d346d1a8165da
RIN67630 <lazlo.lebrun@googlemail.com>
2020-12-19 15:28:17 +0100

    Automatic cartridge reset wo computer / RicohSP200 or SP150 series
    Modified reset.bin.h to contain the original data of  XXL cartridges for Ricoh SP200 and SP150
    Modified Toner_Chip_Reset.ino to upload automatically the original data of  XXL cartridges for Ricoh SP200 or SP150 (depending on a #define in the code) just upon pushing the reset button.

--- Toner_Chip_Reset.ino
+++ Toner_Chip_Reset.ino
@@ -1,22 +1,20 @@
 /*
     FILE: Toner_Chip_Reset.ino
   AUTHOR: Ernesto Lorenz
- VERSION: 0.1
- PURPOSE: Reset chip for the Ricoh Aficio SP-204 toner cartridge
+  VERSION: 0.1
+  PURPOSE: Reset chip for the Ricoh Aficio SP-204 toner cartridge
     DATE: 10.02.2018
-  GITHUB: 
+  GITHUB:
 
- Released to the public domain
- * 
+  Released to the public domain
+
 */
-
+#define RICOH200      // replace by RICHO150 if required
 #include <extEEPROM.h>
 
-// the contents to flash on the eeprom to reset the chip.
-#include "reset_bin.h"
+#include "reset_bin.h" // the contents to flash on the eeprom to reset the chip.
 
-
-const char version[] = "0.1";
+const char version[] = "0.2";
 
 // eeprom address on the i2c bus:
 // 0x53 ( 1010 A0=0 A1=1 A2=1 )
@@ -25,31 +23,31 @@ extEEPROM toner_chip(kbits_2, 1, 8, 0x53);
 
 // dump the content of the eeprom
 void dump(void) {
-   for (unsigned pageaddr  = 0; pageaddr  < 241; pageaddr  += 16) {
-       byte buffer[16]; // Hold a page  of EEPROM (16 bytes)
-       char outbuf[6];  //Room for three hex digits and ':' and ' ' and '\0'
-       sprintf(outbuf, "%03x: ", pageaddr);
-       Serial.print(outbuf);
-       toner_chip.read(pageaddr, buffer, 16);
-       for (int j = 0; j < 16; j++) {
-           if (j == 8) {
-               Serial.print(" ");
-           }
-           sprintf(outbuf, "%02x ", buffer[j]);
-           Serial.print(outbuf);            
-       }
-       Serial.print(" |");
-       for (int j = 0; j < 16; j++) {
-           if (isprint(buffer[j])) {
-               char asciiSTR = toascii(buffer[j]);
-               Serial.print( asciiSTR );
-           }
-           else {
-               Serial.print('.');
-           }
-       }
-       Serial.println("|");
-   }  
+  for (unsigned pageaddr  = 0; pageaddr  < 241; pageaddr  += 16) {
+    byte buffer[16]; // Hold a page  of EEPROM (16 bytes)
+    char outbuf[6];  //Room for three hex digits and ':' and ' ' and '\0'
+    sprintf(outbuf, "%03x: ", pageaddr);
+    Serial.print(outbuf);
+    toner_chip.read(pageaddr, buffer, 16);
+    for (int j = 0; j < 16; j++) {
+      if (j == 8) {
+        Serial.print(" ");
+      }
+      sprintf(outbuf, "%02x ", buffer[j]);
+      Serial.print(outbuf);
+    }
+    Serial.print(" |");
+    for (int j = 0; j < 16; j++) {
+      if (isprint(buffer[j])) {
+        char asciiSTR = toascii(buffer[j]);
+        Serial.print( asciiSTR );
+      }
+      else {
+        Serial.print('.');
+      }
+    }
+    Serial.println("|");
+  }
 }
 
 // initialize serial connection and wait for user input.
@@ -58,18 +56,23 @@ void initialize(void) {
 
   Serial.begin(57600);
   Serial.println("Let's get started");
-  
-  byte i2cStat = toner_chip.begin(toner_chip.twiClock800kHz);
-  
+
+  byte i2cStat = toner_chip.begin(toner_chip.twiClock100kHz);
+
   if ( i2cStat != 0 ) {
     Serial.println("I2C bus init error");
   } else {
-  Serial.println("I2C bus initalized");
+    Serial.println("I2C bus initalized");
   }
 }
 
 void reset(void) {
-  toner_chip.write(0x0, chip_reset, chip_dump_len); //buffered write to the chip
+#ifdef RICOH200 
+  toner_chip.write(0x0, chip_reset200, chip_dump_len); //buffered write to the chip
+#endif
+#ifdef RICOH150 
+  toner_chip.write(0x0, chip_reset150, chip_dump_len); //buffered write to the chip
+#endif
   Serial.println();
   dump();
 }
@@ -91,15 +94,15 @@ void displayHelp()
   Serial.println();
   Serial.println(F("Commands:"));
   Serial.println(F("\td = dump the toner chip contents"));
-  Serial.println(F("\tr = reset the toner chip")); 
+  Serial.println(F("\tr = reset the toner chip"));
   Serial.println(F("\n\t? = help - this page"));
   Serial.println();
 }
 
 void setup(void) {
-  Serial.begin(57600);
+  Serial.begin(9600);
   Serial.println("Initializing I2C bus");
-  byte i2cStat = toner_chip.begin(toner_chip.twiClock800kHz);
+  byte i2cStat = toner_chip.begin(toner_chip.twiClock100kHz);
   if ( i2cStat != 0 ) {
     Serial.println("I2C bus init error");
   }
@@ -107,6 +110,12 @@ void setup(void) {
     Serial.println("I2C bus initalized");
   }
   displayHelp();
+
+  Serial.println("Resetting the chip in 2 seconds");
+  delay(2000);
+  Serial.println("Reseting the toner chip");
+  reset();
+  Serial.println("Reset done");
 }
 
 void loop() {
@@ -125,7 +134,7 @@ void loop() {
     case '?':
       displayHelp;
       break;
-      
+
     default:
       break;
   }
